2bf17b49bc41460d8e21f168083e73bedf62d3d1
Mohan Muppidi <mkumar2301@gmail.com>
2015-11-15 22:22:17 -0600

    Update start_code.sh

--- start_code.sh
+++ start_code.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-python /tensorflow/tensorflow/tensorboard/tensorboard.py --logdir=/tmp/cifar10_train &
+#python /tensorflow/tensorflow/tensorboard/tensorboard.py --logdir=/tmp/cifar10_train &
+
+tensorboard --logdir=/tmp/cifar10_train &
 
 python tensorflow/tensorflow/models/image/cifar10/cifar10_train.py
 
